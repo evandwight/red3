f768c29afd55cfb9afefc722823ce20cafe8c4d7
Evan Dwight <effdem3@gmail.com>
2022-10-24 23:50:34 -0600

    Always keep anime

--- red3/celery.py
+++ red3/celery.py
@@ -20,8 +20,10 @@ app.conf.update(
     beat_schedule={
         'updateAllListing': {'task': 'main.tasks.updateAllListing','schedule': crontab(minute="0,5,10,15,20,25,30,35,40,45,50,55"),},
         'updateSomeComments': {'task': 'main.tasks.updateSomeComments','schedule': 60,},
+        'updateSomeAlwaysKeepComments': {'task': 'main.tasks.updateSomeAlwaysKeepComments','schedule': crontab(minute="3,13,23,33,43,53"),},
         'updatePostCache-hot': {'task': 'main.tasks.updatePostCache', 'args': ['hot'], 'schedule': crontab(minute="1,6,11,16,21,26,31,36,41,46,51,56"),},
         'updatePostCache-new': {'task': 'main.tasks.updatePostCache', 'args': ['new'],'schedule': crontab(minute="2,7,12,17,22,27,32,37,42,47,52,57"),},
+        'updatePostCache-anime': {'task': 'main.tasks.updatePostCache', 'args': ['anime'],'schedule': crontab(minute="4,9,14,19,24,29,34,39,44,49,54,59"),},
         'cleanDb': {'task': 'main.tasks.cleanDb','schedule': crontab(hour=4, minute=0),},
     },
 )
